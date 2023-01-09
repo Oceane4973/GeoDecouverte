29e5b57ee2947fc42dcd8afe2ea76e274f8abd67
LothaireNobili <lothairenobili@gmail.com>
2023-01-09 11:57:07 +0100

    renommage + crea picpic (pour les photos)

--- Application/app/src/main/java/edu/atelier/technique/HomePage.java
+++ Application/app/src/main/java/edu/atelier/technique/HomePage.java
@@ -9,6 +9,10 @@ public class HomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+<<<<<<< Updated upstream:Application/app/src/main/java/edu/atelier/technique/HomePage.java
         setContentView(R.layout.home_page_layout);
+=======
+        setContentView(R.layout.picpic);
+>>>>>>> Stashed changes:Application/app/src/main/java/edu/atelier/technique/MainActivity.java
     }
 }
