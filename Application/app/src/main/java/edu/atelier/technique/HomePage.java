93fa12db687e00e9d708a6488b4aabca3f1fcd16
LothaireNobili <lothairenobili@gmail.com>
2023-01-09 11:58:06 +0100

    correction homepage

--- Application/app/src/main/java/edu/atelier/technique/HomePage.java
+++ Application/app/src/main/java/edu/atelier/technique/HomePage.java
@@ -9,10 +9,8 @@ public class HomePage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< Updated upstream:Application/app/src/main/java/edu/atelier/technique/HomePage.java
+
         setContentView(R.layout.home_page_layout);
-=======
-        setContentView(R.layout.picpic);
->>>>>>> Stashed changes:Application/app/src/main/java/edu/atelier/technique/MainActivity.java
+
     }
 }
