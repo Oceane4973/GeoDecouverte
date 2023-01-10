8a38af947fbdcddb388300d2cc3528c9d70b0ab3
ValentinMontel <113093291+ValentinMontel@users.noreply.github.com>
2023-01-10 13:20:47 +0100

    Commit pour pull origin

--- Application/app/src/main/java/edu/atelier/technique/HomePage.java
+++ Application/app/src/main/java/edu/atelier/technique/HomePage.java
@@ -1,8 +1,12 @@
 package edu.atelier.technique;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page_layout);
+
+        ArrayList<String> listOfPublication = new ArrayList<String>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(HomePage.this,android.R.layout.simple_list_item_1, listOfPublication);
+        ((ListView)findViewById(R.id.listView)).setAdapter(adapter);
     }
 }
---
+++ Application/app/src/main/java/edu/atelier/technique/models/Publication.java
@@ -0,0 +1,14 @@
+package edu.atelier.technique.models;
+
+import java.util.Date;
+
+public class Publication {
+    Integer id;
+    //ImageModel myImage;
+    Boolean isFavoris;
+
+    Publication(/*ImageModel _myImage*/){
+        //this.myImage = _myImage;
+        this.isFavoris = false;
+    }
+}
