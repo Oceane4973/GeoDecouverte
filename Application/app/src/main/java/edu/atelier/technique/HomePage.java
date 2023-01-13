4e9ed7f766a47d858233bae553b2742f63d665f2
ValentinMontel <113093291+ValentinMontel@users.noreply.github.com>
2023-01-13 17:31:57 +0100

    Permissions Camera et Localisation terminées

--- Application/app/src/main/java/edu/atelier/technique/HomePage.java
+++ Application/app/src/main/java/edu/atelier/technique/HomePage.java
@@ -1,11 +1,16 @@
 package edu.atelier.technique;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 import edu.atelier.technique.models.ImageModel;
+import edu.atelier.technique.singletons.ListOfPermissions;
 import edu.atelier.technique.singletons.ListOfPublications;
 import edu.atelier.technique.ui.Adapter.HomePageAdapter;
 import edu.atelier.technique.models.PublicationModel;
@@ -17,6 +22,8 @@ import edu.atelier.technique.ui.Pages.Picpic_activity;
 
 public class HomePage extends AppCompatActivity {
 
+    private static final int LOCATION_PERMISSION = 2000;
+    private static final int EXTERNAL_STORAGE = 3000;
     private ListView simpleList;
     private ArrayList<PublicationModel> publicationList = new ArrayList<PublicationModel>();
 
@@ -24,6 +31,14 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        initiatePermissions();
+
+        if(!ListOfPermissions.getInstance().getlocationPermission()){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION);
+        }
+        if(!ListOfPermissions.getInstance().getExternalStoragePermission()){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, EXTERNAL_STORAGE);
+        }
 
         this.simpleList = (ListView) findViewById(R.id.homePageListView);
 
@@ -51,5 +66,42 @@ public class HomePage extends AppCompatActivity {
         });
         ListOfPublications.getInstance().writeToFile(this);
         ListOfPublications.getInstance().readFromFile(this);
+    }
+
+    private void initiatePermissions(){
+        ListOfPermissions.getInstance().setCameraPermission((ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED));
+        ListOfPermissions.getInstance().setlocalisationPermission(
+                (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) &&
+                (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        );
+        ListOfPermissions.getInstance().setExternalStoragePermission(
+                (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) &&
+                (ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case LOCATION_PERMISSION:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    ListOfPermissions.getInstance().setlocalisationPermission(true);
+                }else{
+                    ListOfPermissions.getInstance().setlocalisationPermission(false);
+                }
+                break;
+
+            case EXTERNAL_STORAGE:
+                if ((grantResults.length > 0) && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    ListOfPermissions.getInstance().setExternalStoragePermission(true);
+                }else{
+                    ListOfPermissions.getInstance().setExternalStoragePermission(false);
+                }
+                break;
+
+            default:
+                break;
+        }
     }
 }
